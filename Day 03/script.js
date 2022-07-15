a525f092e3da1cd4b9a5b5f586c1fa3e0a0e4343
Hijas55 <107784891+Hijas55@users.noreply.github.com>
2022-07-15 18:30:16 +0530

    Add files via upload

---
+++ Day 03/script.js
@@ -0,0 +1,100 @@
+var arr = [
+    {
+      "id": "1",
+      "name": "Lura Senger",
+      "email": "Xander_Collier@yahoo.com",
+      "age": "18"
+    },
+    {
+      "id": "2",
+      "name": "Wilburn Weber",
+      "email": "Bennett_Kreiger11@yahoo.com",
+      "age": "24"
+    },
+    {
+      "id": "3",
+      "name": "Tyrique Hahn",
+      "email": "Juston.Altenwerth@yahoo.com",
+      "age": "30"
+    },
+    {
+      "id": "4",
+      "name": "Lenny Bailey",
+      "email": "Guiseppe_Hegmann@yahoo.com",
+      "age": "27"
+    },
+    {
+      "id": "5",
+      "name": "Vladimir Keeling",
+      "email": "Louisa_Walsh18@hotmail.com",
+      "age": "31"
+    },
+    {
+      "id": "6",
+      "name": "Kellie Crona",
+      "email": "Chandler_Abernathy@yahoo.com",
+      "age": "21"
+    },
+    {
+      "id": "7",
+      "name": "Carolina White",
+      "email": "Royal50@hotmail.com",
+      "age": "28"
+    },
+    {
+      "id": "8",
+      "name": "Alfredo Conn",
+      "email": "Clarabelle34@hotmail.com",
+      "age": "35"
+    },
+    {
+      "id": "9",
+      "name": "Stan Shanahan",
+      "email": "Lamar.McClure@hotmail.com",
+      "age": "25"
+    },
+    {
+      "id": "10",
+      "name": "Marvin Fay",
+      "email": "Osbaldo58@hotmail.com",
+      "age": "29"
+    },
+    {
+      "id": "11",
+      "name": "Torrance Rau",
+      "email": "Orin45@gmail.com",
+      "age": "30"
+    },
+    {
+      "id": "12",
+      "name": "Harold Gutmann MD",
+      "email": "Alyce.Stracke37@yahoo.com",
+      "age": "33"
+    },
+    {
+      "id": "13",
+      "name": "Taryn Torphy",
+      "email": "Dean_Breitenberg71@hotmail.com",
+      "age": "26"
+    },
+    {
+      "id": "14",
+      "name": "Bryana Lang",
+      "email": "Tatum.Ullrich@yahoo.com",
+      "age": "18"
+    },
+    {
+      "id": "15",
+      "name": "Nyasia Green DDS",
+      "email": "Dino83@gmail.com",
+      "age": "34"
+    }
+]
+for (var i = 10; i<15; i++){
+    console.log(arr[i].id, arr[i].name, arr[i].email, arr[i].age);
+}
+
+for (var i in arr) {
+    console.log(arr[i]["id"], arr[i]["name"], arr[i]["email"], arr[i]["age"]);
+}
+
